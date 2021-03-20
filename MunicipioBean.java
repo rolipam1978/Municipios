e7abca8ee5f881919a1d3c9942ba32d3f8cbada7
rolipam1978 <81026882+rolipam1978@users.noreply.github.com>
2021-03-20 12:24:18 -0300

    Add files via upload

---
+++ MunicipioBean.java
@@ -0,0 +1,69 @@
+/**
+ *
+ * @author RAMIRO
+ */
+public class MunicipioBean {
+    
+    private int coduf;
+    private String siglauf;
+    private int codmunic;
+    private String municipio;
+
+    /**
+     * @return the coduf
+     */
+    public int getCoduf() {
+        return coduf;
+    }
+
+    /**
+     * @param coduf the coduf to set
+     */
+    public void setCoduf(int coduf) {
+        this.coduf = coduf;
+    }
+
+    /**
+     * @return the siglauf
+     */
+    public String getSiglauf() {
+        return siglauf;
+    }
+
+    /**
+     * @param siglauf the siglauf to set
+     */
+    public void setSiglauf(String siglauf) {
+        this.siglauf = siglauf;
+    }
+
+    /**
+     * @return the codminic
+     */
+    public int getCodmunic() {
+        return codmunic;
+    }
+
+    /**
+     * @param codminic the codminic to set
+     */
+    public void setCodmunic(int codmunic) {
+        this.codmunic = codmunic;
+    }
+
+    /**
+     * @return the municipio
+     */
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    /**
+     * @param municipio the municipio to set
+     */
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+    
+    
+}
